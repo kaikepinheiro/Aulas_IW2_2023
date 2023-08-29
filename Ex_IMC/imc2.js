b09a31e6b3a56956572d09954d600c1313f104a9
Kaike Pinheiro Simplicio <100208435+kaikepinheiro@users.noreply.github.com>
2023-08-29 11:06:33 -0300

    Add files via upload

---
+++ Ex_IMC/imc2.js
@@ -0,0 +1,24 @@
+function calcula_imc(){
+    var altura = document.imcForm.altura.value;
+    var peso = document.imcForm.peso.value;
+
+    var calculo = (pso/(altura*altura));
+
+    if(calculo<18.5){
+        alert("Você está abaixo do peso com um indice de: " + calculo);
+    }
+    else if(calculo>=18.5 && calculo<24.9){
+        alert("Você está no peso ideal com um indice de: " + calculo);
+    }
+    else if(calculo>=25 && calculo<29.9){
+        alert("Você está com sobrepeso com um indice de: " + calculo);
+    }
+    else if(calculo>=30 && calculo<39.9){
+        alert("Você está com obesiedade com um indice de: " + calculo);
+    }
+    else if(calculo>40){
+        alert("Você está com obesiedade grave com um indice de: " + calculo);
+    }
+
+    document.getElementById('resul').innerHTML = resultado;
+}
